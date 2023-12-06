3a782836be98a83da3b2dd6d1a6e14c53dae393b
era <era@faec.me>
2023-12-06 07:55:21 -0500

    fix virtual qualifiers

--- src/chomp/chomp_plugin.hh
+++ src/chomp/chomp_plugin.hh
@@ -21,23 +21,25 @@ class ChompPlugin : public CLAPPlugin {
  public:
   ChompPlugin(const clap_host_t *_host);
 
-  clap_process_status Process(const clap_process_t *process);
+  virtual clap_process_status Process(const clap_process_t *process);
 
-  bool Init();
-  void Destroy();
-  void Deactivate();
-  bool Activate(
+  virtual bool Init();
+  virtual void Destroy();
+  virtual void Deactivate();
+  virtual bool Activate(
       double sample_rate, uint32_t min_frames_count, uint32_t max_frames_count
   );
-  bool StartProcessing();
-  void StopProcessing();
-  void Reset();
+  virtual bool StartProcessing();
+  virtual void StopProcessing();
+  virtual void Reset();
 
-  uint32_t NotePortsCount(bool is_input);
-  bool NotePortsGet(uint32_t index, bool is_input, clap_note_port_info_t *info);
+  virtual uint32_t NotePortsCount(bool is_input);
+  virtual bool     NotePortsGet(
+          uint32_t index, bool is_input, clap_note_port_info_t *info
+      );
 
-  uint32_t AudioPortsCount(bool is_input);
-  bool     AudioPortsGet(
+  virtual uint32_t AudioPortsCount(bool is_input);
+  virtual bool     AudioPortsGet(
           uint32_t index, bool is_input, clap_audio_port_info_t *info
       );
 
